607da7c09966647ceb5d766c5a7c28fb072cc8d2
Evan Feenstra <evan@evanfeenstra.com>
2019-07-18 12:22:56 -0700

    add comments

--- src/App.js
+++ src/App.js
@@ -10,16 +10,24 @@ class App extends React.Component {
   }
 
   getMemes = async (e) => {
+    // this line prevents the page from reloading (which is the default for <form> elements)
     e.preventDefault()
+    // set "loading" to true in the state so we can show a spinner
     this.setState({loading: true})
+    // here is our giphy api key
     var key = 'jhQazp87aPuMIRIZoFu2kaI2Uk5GjZRJ'
+    // this line make a URL string, I got this from their documentation
     var url = `http://api.giphy.com/v1/gifs/search?q=${this.state.text}&api_key=${key}`
+    // "fetch" calls the giphy API!
     var r = await fetch(url)
+    // this lines extracts JSON (javascript object notation)
     var json = await r.json()
+    // set the memes in state, and loading to false, and the text to blank again
     this.setState({memes: json.data, loading:false, text:''})
   }
 
   render() {
+    // extract the pieces of state so we can use them easily in our HTML code
     var {memes, loading, text} = this.state
     return (
       <div className="App">
@@ -32,15 +40,25 @@ class App extends React.Component {
           </button>
         </form>
         <main>
-          {memes.map(m=>{
-            return <img alt="meme" key={m.id}
-              src={m.images.fixed_height.url}
-            />
+          {memes.map(meme=>{
+            // "map" loops over each meme and returns a <Meme> component
+            return <Meme key={meme.id} meme={meme} />
           })}
         </main>
       </div>
     );
   }
+}
+
+// the Meme component is very simple
+// it just returns a <div> with an <img> inside it
+// the "src" of the <img> is the URL that is given to us by the giphy API
+function Meme(props){
+  const {meme} = props
+  const url = meme.images.fixed_height.url
+  return (<div className="meme-wrap" onClick={()=>window.open(url, '_blank')}>
+    <img height="200" alt="meme" src={url} />
+  </div>)
 }
 
 export default App;
