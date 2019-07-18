bb8d4ba1ce3b197796518262c84531de15536ec8
Evan Feenstra <evan@evanfeenstra.com>
2019-07-17 18:17:01 -0700

    memer

--- src/App.js
+++ src/App.js
@@ -1,26 +1,46 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+
+  state={
+    memes:[],
+    loading:false,
+    text:'',
+  }
+
+  getMemes = async (e) => {
+    e.preventDefault()
+    this.setState({loading: true})
+    var key = 'jhQazp87aPuMIRIZoFu2kaI2Uk5GjZRJ'
+    var url = `http://api.giphy.com/v1/gifs/search?q=${this.state.text}&api_key=${key}`
+    var r = await fetch(url)
+    var json = await r.json()
+    this.setState({memes: json.data, loading:false, text:''})
+  }
+
+  render() {
+    var {memes, loading, text} = this.state
+    return (
+      <div className="App">
+        <form className="App-header" onSubmit={this.getMemes}>
+          <input value={text}
+            onChange={e=> this.setState({text: e.target.value})}
+          />
+          <button disabled={loading || !text} type="submit">
+            Search
+          </button>
+        </form>
+        <main>
+          {memes.map(m=>{
+            return <img alt="meme" key={m.id}
+              src={m.images.fixed_height.url}
+            />
+          })}
+        </main>
+      </div>
+    );
+  }
 }
 
 export default App;
